48140041d35f7a2f18ace1e07cf80b11129d0685
John Doe <hamedhasan422421@gmail.com>
2022-05-30 14:46:15 +0300

    require auth and admin system implement

---
+++ src/Pages/Appointment/Service.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Service = ({ service, setTreatment }) => {
+    const { name, slots,price } = service;
+    return (
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+            <div className="card-body text-center">
+                <h2 className="text-xl font-bold text-secondary">{name}</h2>
+                <p>{
+                    slots.length > 0
+                        ? <span>{slots[0]}</span>
+                    : <span className='text-red-500'>Try another date.</span>
+                }</p>
+                <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
+                <p><small>Price: ${price}</small></p>
+                <div className="card-actions justify-center">
+                    <label
+                        htmlFor="booking-modal"
+                        disabled={slots.length === 0}
+                        onClick={() => setTreatment(service)}
+                        className="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary"
+                    >Book Appointment</label>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
---
+++ src/Pages/Dashboard/Users.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
+import UserRow from './UserRow';
+
+const Users = () => {
+    const {data: users, isLoading, refetch} = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers:{
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+    if(isLoading) {
+        return <Loading/>
+    }
+    return (
+        <>
+                <h2 className="text-2xl">All Users: {users?.length}</h2>
+        <div class="overflow-x-auto">
+        <table class="table w-full">
+         
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+           
+          {
+              users?.map((user, index) => <UserRow
+              key={user._id}
+              user={user}
+              refetch={refetch}
+              index={index}
+              />)
+          }
+       
+          </tbody>
+        </table>
+      </div>
+        </>
+    );
+};
+
+export default Users;
