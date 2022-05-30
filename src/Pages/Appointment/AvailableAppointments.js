fefd9cc3e2474481946987a949c9409168630504
John Doe <hamedhasan422421@gmail.com>
2022-05-30 21:20:18 +0300

    appoinment banner availabe apoinment booking modal service added

---
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -0,0 +1,39 @@
+import { format } from 'date-fns';
+import React, { useState, useEffect } from 'react';
+import BookingModal from './BookingModal';
+import Service from './Service';
+
+const AvailableAppointments = ({ date }) => {
+    const [services, setServices] = useState([]);
+    const [treatment, setTreatment] = useState(null);
+
+    const formattedDate = format(date, 'PP');
+    useEffect(() => {
+        fetch(`http://localhost:5000/available?date=${formattedDate}`)
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
+
+
+    return (
+        <div className='my-10'>
+            <h4 className='text-xl text-secondary text-center my-12'>Available Appointments on {format(date, 'PP')}</h4>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                        setTreatment={setTreatment}
+                    ></Service>)
+                }
+            </div>
+            {treatment && <BookingModal
+                date={date}
+                treatment={treatment}
+                setTreatment={setTreatment}
+            ></BookingModal>}
+        </div>
+    );
+};
+
+export default AvailableAppointments;
