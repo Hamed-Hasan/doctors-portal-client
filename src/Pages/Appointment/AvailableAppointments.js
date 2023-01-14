bd81f445ca15c3ea6cc02e27f0cf601d9a8562f5
John Doe <hamedhasan422421@gmail.com>
2023-01-14 12:27:54 +0300

    modified

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -32,7 +32,7 @@ const AvailableAppointments = ({ date }) => {
         <div className='my-10'>
             <h4 className='text-xl text-secondary text-center my-12'>Available Appointments on {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-                {
+                { services &&
                     services?.map(service => <Service
                         key={service._id}
                         service={service}
